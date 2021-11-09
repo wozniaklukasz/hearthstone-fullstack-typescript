163d4312fad6bc7ffafa75629cf84043ff139e7d
Łukasz Woźniak <wozniak.lukasz.g@gmail.com>
2021-11-09 21:30:51 +0100

    client side prettier and eslint

---
+++ client/.eslintrc.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "react-hooks"],
+  rules: {
+    "@typescript-eslint/no-empty-interface": 0,
+    "react-hooks/rules-of-hooks": 2,
+    "react-hooks/exhaustive-deps": 2,
+  },
+};
