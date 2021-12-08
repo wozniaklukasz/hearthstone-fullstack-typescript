57934ffd4dd00525702e1ac45cf786eccc8adeae
Łukasz Woźniak <wozniak.lukasz.g@gmail.com>
2021-12-08 20:59:03 +0100

    users module - wip

--- packages/server/src/modules/users/users.routes.factory.ts
+++ packages/server/src/modules/users/users.routes.factory.ts
@@ -15,5 +15,13 @@ export class UsersRoutesFactory extends CommonRoutes<IUsersController> {
     this.app.route(`/api/users/:id`).get(async (req: Request, res: Response, next: NextFunction) => {
       await this.controller.getUserById(req, res, next);
     });
+
+    this.app.route(`/api/login`).get(async (req: Request, res: Response, next: NextFunction) => {
+      await this.controller.login(req, res, next);
+    });
+
+    this.app.route(`/api/register`).get(async (req: Request, res: Response, next: NextFunction) => {
+      await this.controller.register(req, res, next);
+    });
   }
 }
